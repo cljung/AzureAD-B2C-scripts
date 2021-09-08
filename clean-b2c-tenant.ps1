b16d74a7b8efb0432df0be8f17f1040a6bf529e1
cljung <cljung@live.se>
2021-09-08 10:57:58 +0200

    empty deleted users

--- clean-b2c-tenant.ps1
+++ clean-b2c-tenant.ps1
@@ -3,6 +3,8 @@
 # This script is intended to reset a B2C demo environment
 # It deletes IEF Keys, IEF Policies, Users (excl GlobalAdmins), Groups and Applications (excl b2c-extensions-app)
 #
+exit # safety switch
+
 $auth =Connect-AzureADB2CDevicelogin -TenantID $global:TenantName -Scope "TrustFrameworkKeySet.ReadWrite.All Policy.ReadWrite.TrustFramework Application.ReadWrite.All User.ReadWrite.All Group.ReadWrite.All"
 $authHeader =@{ 'Content-Type'='application/json'; 'Authorization'=$auth.token_type + ' ' + $auth.access_token }
 
@@ -32,13 +34,22 @@ $globalAdmins = $resp.value
 # delete all users EXCEPT admins (otherwise you would be locked out)
 $url = "https://graph.microsoft.com/beta/users"
 $resp = Invoke-RestMethod -Method GET -Uri $url -Headers $authHeader  
-foreach( $u in $resp.value ) {
+$users = $resp.value
+foreach( $u in $users ) {
     # don't delete Admins
     # if ( !($u.UserPrincipalName.IndexOf("#EXT#") -gt 0 -or $u.UserPrincipalName.StartsWith("graphexplorer@") -eq $True) ) {
     if ( $null -eq ( Compare-Object -IncludeEqual -ExcludeDifferent $u.id $globalAdmins.id) ) {
         $ret = Invoke-RestMethod -Method "DELETE" -Uri "$url/$($u.id)" -Headers $authHeader  
     } else {
         write-host "Skipping Global Admin " $u.id $u.displayName 
+    }
+}
+Sleep 15 # wait for replication
+foreach( $u in $users ) {
+    # don't delete Admins
+    # if ( !($u.UserPrincipalName.IndexOf("#EXT#") -gt 0 -or $u.UserPrincipalName.StartsWith("graphexplorer@") -eq $True) ) {
+    if ( $null -eq ( Compare-Object -IncludeEqual -ExcludeDifferent $u.id $globalAdmins.id) ) {
+        $ret = Invoke-RestMethod -Method "DELETE" -Uri "ttps://graph.microsoft.com/beta/directory/deletedItems/$($u.id)" -Headers $authHeader  
     }
 }
 
@@ -50,10 +61,23 @@ foreach( $g in $resp.value ) {
 }
 
 # delete all applications
+$b2cExtensionsAppId = $null
 $url = "https://graph.microsoft.com/beta/applications"
 $resp = Invoke-RestMethod -Method GET -Uri $url -Headers $authHeader  
 foreach( $a in $resp.value ) {
     if ( !$a.displayName.StartsWith("b2c-extensions-app")) { # do NOT delete this app
         $ret = Invoke-RestMethod -Method "DELETE" -Uri "$url/$($a.id)" -Headers $authHeader  
+    } else {
+        $b2cExtensionsAppId = $a.id
     }
 }
+
+# delete all extensionProperties for b2c-extension-app
+$url = "https://graph.microsoft.com/beta/applications/$b2cExtensionsAppId/extensionProperties"
+$resp = Invoke-RestMethod -Method GET -Uri $url -Headers $authHeader  
+foreach( $g in $resp.value ) {
+    $ret = Invoke-RestMethod -Method "DELETE" -Uri "$url/$($g.id)" -Headers $authHeader  
+}
+
+
+$url = "https://graph.microsoft.com/beta/applications?`$select=id,appId&`$filter=displayName eq '$($app.appName)'"
